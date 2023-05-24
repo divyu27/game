a6e4d2b46ffd5979acc1a454003bd7ccf5457fe2
divyu27 <130134834+divyu27@users.noreply.github.com>
2023-05-25 00:42:32 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 var randomNumber1=Math.floor(Math.random() *6)+1;
 
 var randomDiceImage="dice"+ randomNumber1 + ".png";
-var randomImageSource="images/"+ randomDiceImage;
+var randomImageSource=randomDiceImage;
 
 var image1 = document.querySelectorAll("img")[0];
 
@@ -11,7 +11,7 @@ image1.setAttribute("src",randomImageSource);
 var randomNumber2=Math.floor(Math.random() *6)+1;
 
 var randomDiceImage2="dice"+ randomNumber2 + ".png";
-var randomImageSource2="images/"+ randomDiceImage2;
+var randomImageSource2=randomDiceImage2;
 
 var image2 = document.querySelectorAll("img")[1];
 
